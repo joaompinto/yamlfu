fac5595fc368d40d8fc0db24971995baab7c3499
João Pinto <lamego.pinto@gmail.com>
2020-10-17 14:12:25 +0200

    Automated release using github actions

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-from setuptools import setup
-import versioneer
-import sys
-import os
 import io
+from pathlib import Path
+from setuptools import setup
 
-assert sys.version_info >= (3, 6, 0), "requires Python 3.6+"
-from pathlib import Path  # noqa E402
-
-CURRENT_DIR = Path(__file__).parent
+SCRIPT_DIR = Path(__file__).parent
 
 
 def setup_package():
 
     # Get readme
-    readme_path = os.path.join(CURRENT_DIR, "README.md")
+    readme_path = Path(SCRIPT_DIR, "README.md")
     with io.open(readme_path, encoding="utf8") as f:
         readme = f.read()
 
@@ -22,14 +17,10 @@ def setup_package():
         requirements = f.read()
 
     setup(
-        version=versioneer.get_version(),
+        use_scm_version=True,
         long_description=readme,
         long_description_content_type="text/markdown",
         install_requires=[x for x in requirements.splitlines() if x],
-        package_data={
-            # If any package contains *.json files, include them:
-            "": ["*.json"]
-        },
     )
 
 
